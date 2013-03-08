42b6c049abe32c5baf9fea31437084464634f205
mojzis <m@pippi>
2013-03-08 15:36:53 +0000

    merged master

--- js/droprx.js
+++ js/droprx.js
@@ -26,6 +26,10 @@ dropMeta.config(function ($routeProvider) {
       templateUrl: 'partials/hooks.html',
       controller: 'HooksCtrl'
     })      
+    .when('/styles', {
+      templateUrl: 'partials/styles.html',
+      controller: 'StylesCtrl'
+    })      
     .otherwise({
       redirectTo: '/about'
     });
@@ -45,40 +49,57 @@ dropMeta.directive('dmPropTable', function(){
 
 
 function MainMenu($scope) {
-  $scope.menu = ['about', 'fields', 'ents', 'hooks','modules']
+  $scope.menu = ['about', 'fields', 'ents', 'hooks','modules','styles'];
 }
 
-function AboutCtrl($scope, Drupal) {}
+function AboutCtrl($rootScope, $scope, Drupal) {
+  
+}
 
-function HooksCtrl($scope, Drupal) {}
+function HooksCtrl($rootScope, $scope, Drupal) {}
 
-function EntsCtrl($scope, Drupal) {
-    $scope.ents = Drupal.query({what: 'ents'});
-    $scope.selectRow = function(index) {
-      $scope.selrow = $scope.ents[index];
-      console.log(index);
-    }
+function EntsCtrl($rootScope, $scope, Drupal) {
+  $scope.ents = Drupal.query({what: 'ents'});
+  $scope.selectRow = function(index) {
+    $scope.selrow = $scope.ents[index];
   }
+}
 
-function FieldsCtrl($scope, Drupal) {
-    $scope.rowClass = new Object();
-    $scope.fields = Drupal.query({what: 'fields'});
-    $scope.selectRow = function(index) {
-      $scope.selrowProps = new Object();
-      angular.extend($scope.selrowProps, $scope.fields[index]);
-      $scope.selrowStore = $scope.selrowProps.storage;
-      if($scope.selrowProps.storage) {
-        $scope.selrowStoreDet = $scope.selrowProps.storage.details;
-        delete $scope.selrowStore.details;
-      }
-      $scope.selrowCols = $scope.selrowProps.columns;
-      $scope.selrowSet = $scope.selrowProps.settings;
-      $scope.selrowDet = $scope.selrowProps.details;
-      delete $scope.selrowProps.storage;
-      
-      delete $scope.selrowProps.columns;
-      delete $scope.selrowProps.settings;
-      delete $scope.selrowProps.details;
-      $scope.rowClass.index = 'selectedRow';
+function StylesCtrl($rootScope, $scope, Drupal) {
+  $scope.styles = Drupal.query({what: 'styles'});
+  $scope.selectRow = function(index) {
+    $scope.selectedRow = index;
+    $scope.selrowProps = new Object();
+    angular.extend($scope.selrowProps, $scope.styles[index]);
+    $scope.selrowEffects = new Object();
+    angular.forEach($scope.selrowProps.effects, function(eff) {
+      $scope.selrowEffects[eff.label] = eff.data
+    })
+    
+    delete $scope.selrowProps.effects;
+  }  
+}
+
+function FieldsCtrl($rootScope, $scope, Drupal) {
+  $scope.selectedRow = 0;
+  $scope.fields = Drupal.query({what: 'fields'});
+  $scope.selectRow = function(index) {
+    $scope.selectedRow = index;
+    $scope.selrowProps = new Object();
+    // todo : find a way for the other data to be cleared on each row, too (selrowCols ...)
+    // or do a transform immediately upon receiving the data ?
+    angular.extend($scope.selrowProps, $scope.fields[index]);
+    $scope.selrowStore = $scope.selrowProps.storage;
+    if($scope.selrowProps.storage) {
+      $scope.selrowStoreDet = $scope.selrowProps.storage.details;
+      delete $scope.selrowStore.details;
     }
-  };
+    $scope.selrowCols = $scope.selrowProps.columns;
+    $scope.selrowSet = $scope.selrowProps.settings;
+    $scope.selrowDet = $scope.selrowProps.details;
+    delete $scope.selrowProps.storage;
+    delete $scope.selrowProps.columns;
+    delete $scope.selrowProps.settings;
+    delete $scope.selrowProps.details;
+  }
+};
